0b51fac12276c7cf2b465db559dad6b448a1c0ac
geragd23 <144053858+geragd23@users.noreply.github.com>
2023-12-03 20:01:16 -0500

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,46 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
+initializeCarousel("carousel1");
+initializeCarousel("carousel2");
+initializeCarousel("carousel3");
+initializeCarousel("carousel4");
+initializeCarousel("carousel5");
+initializeCarousel("carousel6");
+initializeCarousel("carousel7");
+initializeCarousel("carousel8");
+initializeCarousel("carousel9");
+initializeCarousel("carousel10");
+initializeCarousel("carousel11");
+
+
+
+
 function openNav() {
     document.getElementById("nav-links").style.width = "350px";
     document.getElementById("main").style.marginLeft = "350px";
@@ -12,3 +55,4 @@ function closeNav() {
     document.getElementById("nav-action").style.display = "block";
     document.getElementById("nav-logo").style.textAlign = "block";
   }
+
